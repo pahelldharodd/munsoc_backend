64a7a0aa7facd105c5fe2881bedaa6357dd39852
YatharthVora <yatharthvora15@gmail.com>
2025-04-06 12:46:23 +0530

    [update]

--- api_proj/main.py
+++ api_proj/main.py
@@ -4,11 +4,11 @@ from . import models  # Import from models.py
 app = FastAPI()
 todos = []
 
-@app.get("/Todo", response_model=list[models.Todo], tags=["Read"])
+@app.get("/Todo", response_model=list[models.Todo], tags=["Read"],responses={"200":"Succefully executed","404":"List Empty"})
 async def send_todos():
     return todos
 
-@app.post("/Todo", response_model=models.Todo, tags=["Create"])
+@app.post("/Todo", response_model=models.Todo, tags=["Create"],response_description=(),responses={"200":"Succefully executed","400 ":"Already Exsist"})
 async def create(todo: models.Todo):
     if any(t.name == todo.name for t in todos):
         raise HTTPException(status_code=400, detail=f"Todo with name '{todo.name}' already exists")
@@ -47,8 +47,5 @@ async def delete_todo(name: str):
     for i, todo in enumerate(todos):
         if todo.name == name:
             deleted = todos.pop(i)
-            return {
-                "message": f"Todo '{name}' deleted successfully",
-                "deleted": deleted
-            }
+            return deleted
     raise HTTPException(status_code=404, detail=f"Todo with name '{name}' not found")
